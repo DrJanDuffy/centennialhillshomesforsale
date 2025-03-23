20b158c72463456809a0e5b279b1872d760d2504
Dr. Janet Duffy REALTOR License# S.0197614.LLC <DrDuffy@bhhsnv.com>
2025-03-23 05:59:08 -0700

    Add TypeScript declarations for RealScout components

---
+++ types/global.d.ts
@@ -0,0 +1,11 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    'realscout-office-listings': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & {
+      'agent-encoded-id': string;
+      'sort-order': string;
+      'listing-status': string;
+      'property-types': string;
+      'price-min': string;
+    }, HTMLElement>;
+  }
+} 
