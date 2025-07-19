d6d161fd743bc8a0d80503a88b3079a6ef354617
genekellyboyle <geneboyle@gmail.com>
2025-07-19 00:10:50 -0700

    Fix parsing error in InteractivePropertyMap component and resolve build issues

--- components/InteractivePropertyMap.tsx
+++ components/InteractivePropertyMap.tsx
@@ -98,7 +98,7 @@ const InteractivePropertyMap: React.FC<InteractivePropertyMapProps> = ({ classNa
       <div className="relative h-96 bg-gradient-to-br from-blue-50 to-indigo-100">
         {/* Background Map Pattern */}
         <div className="absolute inset-0 opacity-20">
-          <div className="w-full h-full bg-[url('data:image/svg+xml,%3Csvg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"%3E%3Cg fill="none" fill-rule="evenodd"%3E%3Cg fill="%239C92AC" fill-opacity="0.1"%3E%3Ccircle cx="30" cy="30" r="2"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')]"></div>
+          <div className="w-full h-full bg-gradient-to-br from-blue-100 to-indigo-200"></div>
         </div>
 
         {/* Neighborhood Markers */}
